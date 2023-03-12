b6436196be5e4b4fdc97ba46328265bfc087ac9b
Patryk Pudełko <kartonik.pp@gmail.com>
2023-03-12 17:11:49 +0100

    Dodano funkcję run_tests(), która służy do testowania funkcji solve_problem na różnych rozmiarach danych wejściowych generowanych przez funkcję generate_data.

--- src/algo/z2/task1.py
+++ src/algo/z2/task1.py
@@ -6,3 +6,26 @@ from random import seed, randint
 def generate_data(data_size):
     seed(111)
     return [randint(0, 10 ** 6) for _ in range(data_size)]
+
+
+
+def run_tests(generator, solver):
+    size = 10
+    sizes = []
+    times = []
+    while size < 1000:
+        print(f'testing solver for {size=}')
+        data1,data2 = generator(size),generator(size)
+        REPETITIONS = 400
+        time_sum = 0
+        for i in range(REPETITIONS):
+            st = datetime.now().timestamp()
+            ret = solver(data1,data2)
+            en = datetime.now().timestamp()
+            time_sum += (en - st)
+
+        sizes.append(size)
+        times.append(time_sum / REPETITIONS * 10**6)
+        size *= 2
+
+    return sizes, times
