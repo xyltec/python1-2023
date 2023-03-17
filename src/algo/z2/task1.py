513961fb399fc1f59edffe75284f51c2e4b70742
Emilia Raduszewska <emilka.raduszewska@gmail.com>
2023-03-17 17:01:50 +0100

    Update task1.py

--- src/algo/z2/task1.py
+++ src/algo/z2/task1.py
@@ -7,7 +7,15 @@ def generate_data(data_size):
     seed(111)
     return [randint(0, 10 ** 6) for _ in range(data_size)]
 
-
+def solve_problem(a,b):
+    w = []
+    for num in a:
+        counter = 0
+        for num2 in b:
+            if num2 % num == 0:
+                counter += 1
+        w.append(counter)
+    return w
 
 def run_tests(generator, solver):
     size = 10
@@ -32,3 +40,8 @@ def run_tests(generator, solver):
 
 if __name__ == '__main__':
     x, y = run_tests(generate_data, solve_problem)
+
+    plt.plot(x,y)
+    plt.xlabel("Rozmiar danych")
+    plt.ylabel("Czas wykonania (usec)")
+    plt.show
