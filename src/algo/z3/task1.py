b8d3000bb8af132bc4a1d7823cd33636fccb3059
xyltec <125214164+xyltec@users.noreply.github.com>
2023-03-23 21:08:30 +0100

    task: best multiply

---
+++ src/algo/z3/task1.py
@@ -0,0 +1,36 @@
+from random import seed, randint
+
+def run_tests(generator, solver, step_factor=1.3, max_size=10 ** 5):
+    size = 10
+    sizes = []
+    times = []
+    while size < max_size:
+        print(f'testing solver for {size=:.0f} ...', end=' ')
+        data = generator(int(size))
+        REPETITIONS = 0
+        time_sum = 0
+        while True:
+            st = datetime.now().timestamp()
+            ret = solver(**data)
+            en = datetime.now().timestamp()
+            time_sum += (en - st)
+            REPETITIONS += 1
+            if time_sum > 0.1:
+                break
+
+        sizes.append(size)
+        exec_time = time_sum / REPETITIONS * 10 ** 6
+        print(f' execution time: {int(exec_time / 10**3)}ms')
+        times.append(exec_time)
+        size *= step_factor
+
+    return sizes, times
+
+
+def solve(a: list[int]) -> int:
+    max_result = 0
+    for i in range(len(a)-1):
+        res = a[i] * a[i+1]
+        if res > max_result:
+            max_result = res
+    return max_result
