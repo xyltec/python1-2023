572eec344712eebd53ba45066fe3f43b68c32da8
Piotr Marecki <pmarecki@gmail.com>
2023-03-25 15:15:59 +0100

    feat: star-problem for trains

---
+++ src/algo/z4/trains/d.py
@@ -0,0 +1,10 @@
+"""
+zadanie4(*):
+ Mając dany "rozkład jazdy" (z pliku gen_train_data.py), napisać funkcję która znajdzie najdłuższą możliwą
+ trasę (z przesiadkami), w której nie jedziemy przez żadne z miast więcej niż raz.
+
+"""
+
+
+def find_longest_trip(train_data: list[tuple[int, int]]) -> bool:
+    pass
