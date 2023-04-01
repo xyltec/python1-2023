5fc39648f480df681126f480d07d01d17559848c
Piotr Marecki <pmarecki@gmail.com>
2023-04-01 14:56:32 +0200

    feat: some example train data

--- src/algo/z4/trains/zadanie_d.py
+++ src/algo/z4/trains/zadanie_d.py
@@ -4,11 +4,14 @@ zadanie4(*):
  trasę (z przesiadkami), w której nie jedziemy przez żadne z miast więcej niż raz.
 
 """
-from zadanie_a import extract_destination_info
+from random import seed
+
+from gen_train_data import generate_data
+from zadanie_a import extract_destination_info, print_destinations
+
 
 def find_longest_trip(train_data: list[tuple[int, int]]) -> bool:
     pass
-
 
 
 def random_travel(at: int, destinations: list[list[int]]):
@@ -16,4 +19,10 @@ def random_travel(at: int, destinations: list[list[int]]):
 
 
 if __name__ == '__main__':
-    destinations = extract_destination_info()
+    seed(107)
+    # train_data = [(1, 2), (2, 4), (4, 3), (3, 1), (3, 5), (5, 1)]
+    # train_data = [(1, 2), (2, 4), (4, 3), (3, 1)]
+    train_data = generate_data(25)
+
+    destinations = extract_destination_info(train_data)
+    print_destinations(destinations)
