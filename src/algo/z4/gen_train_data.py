ed299bfe82ff6889ff6ac02c597c24cf7a67b7e2
xyltec <125214164+xyltec@users.noreply.github.com>
2023-04-05 00:06:24 +0200

    generate_data, but shorter

--- src/algo/z4/gen_train_data.py
+++ src/algo/z4/gen_train_data.py
@@ -3,13 +3,8 @@ from random import randint, seed
 N_CITIES = 20
 seed(111)
 
-
 def generate_data(data_size):
-    res = []
-    for i in range(data_size):
-        res.append((randint(0, N_CITIES - 1), randint(0, N_CITIES - 1)))
-    return res
-
+    return [(randint(0, N_CITIES - 1), randint(0, N_CITIES - 1)) for i in range(data_size)]
 
 if __name__ == '__main__':
     rr = generate_data(10)
