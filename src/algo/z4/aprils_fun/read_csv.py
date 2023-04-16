80bc4c2cfdf7ebf4ddc3f031c76dcaa5b547e9e7
Piotr Marecki <pmarecki@gmail.com>
2023-04-16 11:23:08 +0200

    feat: add few examples on how to use (0.001% of features of) pandas

--- src/algo/z4/aprils_fun/read_csv.py
+++ src/algo/z4/aprils_fun/read_csv.py
@@ -1,14 +1,48 @@
-
 import pandas as pd
 
-
 # projekt wymaga biblioteki pandas; instalacja `pip install pandas`
-
+# more: https://pandas.pydata.org/docs/user_guide/10min.html
 
 df = pd.read_csv('data/tmdb_5000_movies.csv')
-print(df['title'])
+# print(df.dtypes)
+"""
+budget                    int64
+genres                   object
+homepage                 object
+id                        int64
+keywords                 object
+original_language        object
+original_title           object
+overview                 object
+popularity              float64
+production_companies     object
+production_countries     object
+release_date             object
+revenue                   int64
+runtime                 float64
+spoken_languages         object
+status                   object
+tagline                  object
+title                    object
+vote_average            float64
+vote_count                int64
+"""
+# print(type(df['title']))  # pandas.core.series.Series
 
-titles = list(df['title'])
-# print(titles)
+# print(df.index)  # RangeIndex(start=0, stop=4803, step=1)
+# print(df.describe())  #średnie, kwantyle etc
 
-print(df.head())
+# print(df['genres'][0:10])   # 10 rzędów z kolumny 'genres'
+
+df_sub = df.loc[:, ['title', 'homepage', 'tagline']]  # wycinek tabel
+print(type(df))  # <class 'pandas.core.frame.DataFrame'>
+print(type(df_sub))  # <class 'pandas.core.frame.DataFrame'>
+
+df_as_dict = df_sub.to_dict(orient='records')
+print(type(df_as_dict))  # list
+print(type(df_as_dict[0]))  # dict,
+print(df_as_dict[0])
+# {'title': 'Avatar', 'homepage': 'http://www.avatarmovie.com/', 'tagline': 'Enter the World of Pandora.'}
+
+for movie in df_as_dict:
+    print(f"{movie['title']}\t tag:{movie['tagline']}")
