7598f76b7bb4932dfd928be39ff90c309f87299f
xyltec <125214164+xyltec@users.noreply.github.com>
2023-05-06 15:12:45 +0200

    Update helper.py

--- src/algo/z6_t0/helper.py
+++ src/algo/z6_t0/helper.py
@@ -3,8 +3,10 @@ from model import Fraction
 
 
 def add(f1: Fraction, f2: Fraction) -> Fraction:
-    a = f1.numer()
-    b = f1.denom()
-    c = f2.numer()
-    d = f2.denom()
-    return (a*d + c * b) / (b * d)
+    a = f1.get_numer()
+    b = f1.get_denom()
+    c = f2.get_numer()
+    d = f2.get_denom()
+    numer = a * d + c * b
+    denom = b * d
+    return Fraction(numer, denom)
