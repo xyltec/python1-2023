0d5255a0dc8599e59c3cf37f7dffefd575c128da
xyltec <125214164+xyltec@users.noreply.github.com>
2023-05-13 13:47:25 +0200

    Update trains_a.py

--- src/algo/z4/trains_a.py
+++ src/algo/z4/trains_a.py
@@ -1,5 +1,24 @@
 from gen_train_data import generate_data
 
+def print_destinations(destinations: list[list[int]]):
+    for i, dest in enumerate(destinations):
+        print(i, '->', dest)
+
+
+def extract_destination_info(train_data: list[tuple[int, int]]):
+    """
+    Turns a list of [from,to] tuples into an info of the sort
+    city -> list of destinations reachable from the city
+
+    :param train_data:
+    :return:
+    """
+    max_city = max([start for start, end in train_data])
+    destinations = [[] for _ in range(max_city + 1)]
+    for start, end in train_data:
+        destinations[start].append(end)
+    return destinations
+
 def get_city_most_connections(train_data: list[tuple[int,int]])->int:
     counter = {}
     for train in train_data:
