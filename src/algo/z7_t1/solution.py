e9be23761f48a25899ebf2949255beb2d6b09473
xyltec <kryon48@gmail.com>
2023-05-20 15:09:19 +0200

    Fix z7 folder

--- src/algo/z7_t1/solution.py
+++ src/algo/z7_t1/solution.py
@@ -1,23 +1,36 @@
-
 """
-Słowa sklejamy z sylab w następujący sposób:
+****     #
+*     ####
+*       ##
+***     ##
+*     ####
+**     ###
 
-słowo:
+jeśli długości *'ek klucza + długości #'ów zamka są dla wszystkich takie same, to klucz otwiera zamek
 
-"abac"
-
-... trzeba złożyć z 2-literowych "sylab"
-ab←→ba -> aba
-aba←→ac -> abac
-
-(czyli dołączana sylaba 2-literowa ma mieć taką pierwszą literę, jak ostatnia litera słowa do którego jest dołączana)
 
 """
 
 
-def split_to_syllables(word: str) -> list[str]:
-    sylaby = []
-    for i in range(len(word) - 1):
-        sylaba = word[i:i+2]
-        sylaby.append(sylaba)
-    return sylaby
+
+
+def open_lock(key: list[int], lock: list[int]) -> int:
+    """
+    :param key:   np. [4,1,1,3,1,2]
+    :param lock:  np  [1,4,2,2,4,3]
+    :return:    -1 jeśli wartości są "invalid" (np. <0); 0 jeśli klucz otwiera zamek, 1 jeśli klucz nie otwiera zamka
+    """
+    if len(key) == 0 or len(lock) == 0 or len(key) != len(lock):
+        return -1
+    else:
+        required_sum = key[0] + lock[0]
+        for i in range(len(key)):
+            if key[i] < 0 or lock[i] < 0:
+                return -1
+            elif key[i] + lock[i] != required_sum:
+                return 1
+        return 0
+
+
+if __name__ == "__main__":
+    print(open_lock([4,1,1,3,1,2],[1,4,2,2,4,3]))
