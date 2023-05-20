cc431ae0a7667ad024d25fa922553587ef56f0f3
Piotr Marecki <pmarecki@gmail.com>
2023-05-20 14:58:43 +0200

    feat: add template for task 2

--- src/algo/z7_t1/solution.py
+++ src/algo/z7_t1/solution.py
@@ -1,20 +1,43 @@
 
 """
-Słowa sklejamy z sylab w następujący sposób:
+****     #
+*     ####
+*       ##
+***     ##
+*     ####
+**     ###
 
-słowo:
+jeśli długości *'ek klucza + długości #'ów zamka są dla wszystkich takie same, to klucz otwiera zamek
 
-"abac"
-
-... trzeba złożyć z 2-literowych "sylab"
-ab←→ba -> aba
-aba←→ac -> abac
-
-(czyli dołączana sylaba 2-literowa ma mieć taką pierwszą literę, jak ostatnia litera słowa do którego jest dołączana)
 
 """
 
 
-def split_to_syllables(word: str) -> list[str]:
-    #todo: your solution here
-    return ["aa", "ab", "bb"]
+
+def open_lock(key: list[int], lock: list[int]) -> int:
+
+    if len(key) != len(lock):
+        return -1
+
+    try:
+        x = key[0]+lock[0]
+    except IndexError:
+        return -1
+    for i in range(len(key)):
+        if key[i] < 0 or lock[i] < 0:
+            return -1
+            break
+        elif key[i] >= 0 and lock[i] >= 0:
+            for j in range(len(key)):
+                if key[j]+lock[j] != x:
+                    return 1
+                    break
+        return 0
+
+    """
+    :param key:   np. [4,1,1,3,1,2]
+    :param lock:  np  [1,4,2,2,4,3]
+    :return:    -1 jeśli wartości są "invalid" (np. <0); 0 jeśli klucz otwiera zamek, 1 jeśli klucz nie otwiera zamka
+    """
+# result = open_lock(key, lock)
+# print(result)
--- src/algo/z8/solution2.py
+++ src/algo/z8/solution2.py
@@ -12,9 +12,12 @@ aba←→ac -> abac
 
 (czyli dołączana sylaba 2-literowa ma mieć taką pierwszą literę, jak ostatnia litera słowa do którego jest dołączana)
 
+Zadanie -- mamy dostępny zbiór sylab, oraz pewne słowo `word`; pytanie -- czy przy użyciu tych sylab
+(każdą można użyć dowolną liczbę razy, również 0) można utworzyć dane słowo.
+
 """
 
 
-def split_to_syllables(word: str) -> list[str]:
+def construct_word(syllables: set[str], word: str) -> bool:
     #todo: your solution here
-    return ["aa", "ab", "bb"]
+    return True
