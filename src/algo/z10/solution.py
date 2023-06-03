18a55df2613fa6441075e333d33a828b43d26705
xyltec <125214164+xyltec@users.noreply.github.com>
2023-06-03 16:34:00 +0200

    Update solution.py

--- src/algo/z10/solution.py
+++ src/algo/z10/solution.py
@@ -5,7 +5,7 @@ def longest_sequence(s: str, symbol: str)->int:
     if len(s) == 0: return 0
 
     unwanted_char_check = ''.join([x for x in s if x in ["<",">"]]) #just to make sure there is nothing else than "<" and ">"
-    if len(unwanted_char_check) != len(s): return -1
+    if len(unwanted_char_check) != len(s): return error
     
     if symbol in ["<",">"]:
         if symbol == ">":
