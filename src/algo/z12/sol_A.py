377921c9342ed7bf08bef09a895e3cbf102cbde6
Piotr Marecki <pmarecki@gmail.com>
2023-06-24 14:14:06 +0200

    feat: add stub for A and tests

---
+++ src/algo/z12/sol_A.py
@@ -0,0 +1,3 @@
+def get_min_number_of_operations(a: list[int]) -> int:
+    # todo: your code here
+    return 0
---
+++ src/algo/z12/tests_A.py
@@ -0,0 +1,27 @@
+import unittest
+from sol_A import *
+
+
+class SyllablesTest(unittest.TestCase):
+
+    def test_1(self):
+        self.assertEquals(get_min_number_of_operations([1, 0, -1, -1, 0, 2, 5, 0, -2]), 2)
+
+    def test_2(self):
+        self.assertEquals(get_min_number_of_operations([-1, 1, -1]), 2)
+
+    def test_3(self):
+        self.assertEquals(get_min_number_of_operations([-1, 1, 2, 3]), 1)
+
+    def test_4(self):
+        self.assertEquals(get_min_number_of_operations([-1, 0, -1]), 1)
+
+    def test_5(self):
+        self.assertEquals(get_min_number_of_operations([-1, 1, -1, 1, -1, 1, -1]), 4)
+
+    def test_6(self):
+        self.assertEquals(get_min_number_of_operations([1, -1, -2, -3, -4]), 1)
+
+
+if __name__ == '__main__':
+    unittest.main()
