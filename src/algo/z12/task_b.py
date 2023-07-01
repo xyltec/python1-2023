915f7633810f7e00fa83481babd1b3c4b9256ea3
xyltec <125214164+xyltec@users.noreply.github.com>
2023-07-01 13:47:13 +0200

    Update task_b.py

--- src/algo/z12/task_b.py
+++ src/algo/z12/task_b.py
@@ -1,4 +1,12 @@
 def get_best_value_package(costs: list[int], values: list[int], max_total_cost: int) -> int:
-    # znaleźć najlepszą możliwą wartość podzbioru itemów, ale takich by łączny "cost" tych itemów nie przekraczał
-    # wartości max_total_cost
-    pass
+    n = len(costs)
+    dp = [[0] * (max_total_cost + 1) for _ in range(n + 1)]
+
+    for i in range(1, n + 1):
+        for j in range(1, max_total_cost + 1):
+            if costs[i-1] > j:
+                dp[i][j] = dp[i-1][j]
+            else:
+                dp[i][j] = max(dp[i-1][j], values[i-1] + dp[i-1][j - costs[i-1]])
+
+    return dp[n][max_total_cost]
